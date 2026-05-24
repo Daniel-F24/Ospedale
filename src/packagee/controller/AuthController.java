f287ee0c1c2d7570fbc6e82176c385851b91b5e7
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:25:21 -0500

    Update AuthController.java

--- src/packagee/controller/AuthController.java
+++ src/packagee/controller/AuthController.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.controller;
 
-/**
- *
- * @author danip
- */
+import packagee.dto.UserDTO;
+import packagee.request.LoginRequest;
+import packagee.response.Response;
+import packagee.service.AuthService;
+
 public class AuthController {
-    
+
+    private final AuthService authService;
+    private UserDTO currentUser;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public Response<UserDTO> login(LoginRequest request) {
+        Response<UserDTO> response = authService.login(request);
+        if (response.isSuccess()) {
+            currentUser = response.getData();
+        }
+        return response;
+    }
+
+    public Response<String> logout() {
+        currentUser = null;
+        return Response.ok("Sesion cerrada correctamente.", "LOGOUT");
+    }
+
+    public UserDTO getCurrentUser() {
+        return currentUser;
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public boolean isAdminSession() {
+        return currentUser != null && "ADMINISTRATOR".equalsIgnoreCase(currentUser.getRole());
+    }
 }
+
