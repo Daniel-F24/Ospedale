11c24d19562687a12275a78fd0d05b382fd1dd54
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:28:42 -0500

    Update ModelEvent.java

--- src/packagee/observer/ModelEvent.java
+++ src/packagee/observer/ModelEvent.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.observer;
 
-/**
- *
- * @author danip
- */
+
 public class ModelEvent {
-    
+
+    private final ModelEventType type;
+    private final String source;
+    private final String description;
+
+    public ModelEvent(ModelEventType type, String source, String description) {
+        this.type = type;
+        this.source = source;
+        this.description = description;
+    }
+
+    public ModelEventType getType() {
+        return type;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
