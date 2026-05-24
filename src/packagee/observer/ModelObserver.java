97192e52fb654b0052d2407e7475aebc2d7d8c50
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:29:42 -0500

    Update ModelObserver.java

--- src/packagee/observer/ModelObserver.java
+++ src/packagee/observer/ModelObserver.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.observer;
 
-/**
- *
- * @author danip
- */
-public class ModelObserver {
-    
+
+public interface ModelObserver {
+    void onModelChanged(ModelEvent event);
 }
