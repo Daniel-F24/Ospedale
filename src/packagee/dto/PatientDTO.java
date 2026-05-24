803eae9d3979d785678041d4c18a3ef8dd9890d6
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:20:32 -0500

    Update PatientDTO.java

--- src/packagee/dto/PatientDTO.java
+++ src/packagee/dto/PatientDTO.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.dto;
 
-/**
- *
- * @author danip
- */
-public class PatientDTO {
-    
+
+public class PatientDTO extends UserDTO {
+
+    private final String email;
+    private final String birthdate;
+    private final String gender;
+    private final String phone;
+    private final String address;
+
+    public PatientDTO(long id, String username, String firstname, String lastname,
+                      String email, String birthdate, String gender, String phone, String address) {
+        super(id, username, firstname, lastname, "PATIENT");
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
