83a7a08b5223f09060eb74af184ea66138f10277
Daniel-F24 <danyrespawn@gmail.com>
2026-05-24 17:46:05 -0500

    Update AppointmentStatus.java

--- src/packagee/model/enums/AppointmentStatus.java
+++ src/packagee/model/enums/AppointmentStatus.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package packagee.model.enums;
 
-/**
- *
- * @author edangulo
- */
 public enum AppointmentStatus {
-    
-    REQUESTED, 
-    PENDING, 
-    COMPLETED, 
+    REQUESTED,
+    PENDING,
+    COMPLETED,
     CANCELED
-    
 }
