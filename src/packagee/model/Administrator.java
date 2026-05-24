41bcdd45e408d9dd2efdb52403160d8cefc43bfd
Daniel-F24 <danyrespawn@gmail.com>
2026-05-24 17:42:55 -0500

    Update Administrator.java

--- src/packagee/model/Administrator.java
+++ src/packagee/model/Administrator.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.model;
 
-import packagee.model.User;
 
-/**
- *
- * @author edangulo
- */
 public class Administrator extends User {
-    
+
     public Administrator(long id, String username, String firstname, String lastname, String password) {
         super(id, username, firstname, lastname, password);
     }
-    
 }
