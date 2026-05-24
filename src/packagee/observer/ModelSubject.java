99d8f283a13afc68a924833bc15e95c55cbb9e5e
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:29:55 -0500

    Update ModelSubject.java

--- src/packagee/observer/ModelSubject.java
+++ src/packagee/observer/ModelSubject.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.observer;
 
-/**
- *
- * @author danip
- */
-public class ModelSubject {
-    
+public interface ModelSubject {
+    void addObserver(ModelObserver observer);
+    void removeObserver(ModelObserver observer);
+    void notifyObservers(ModelEvent event);
 }
+
