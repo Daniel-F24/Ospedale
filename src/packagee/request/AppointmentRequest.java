111f44018909bdc10eaef553ce303bbdcee77913
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:28:57 -0500

    Update AppointmentRequest.java

--- src/packagee/request/AppointmentRequest.java
+++ src/packagee/request/AppointmentRequest.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.request;
 
-/**
- *
- * @author danip
- */
 public class AppointmentRequest {
-    
+
+    private final String patientId;
+    private final String doctorId;
+    private final String specialty;
+    private final String date;
+    private final String time;
+    private final String reason;
+    private final String type;
+
+    public AppointmentRequest(String patientId, String doctorId, String specialty,
+                              String date, String time, String reason, String type) {
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.specialty = specialty;
+        this.date = date;
+        this.time = time;
+        this.reason = reason;
+        this.type = type;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
+
