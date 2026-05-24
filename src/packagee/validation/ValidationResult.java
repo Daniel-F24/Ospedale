530d1d558a08b7f72f65e39230c31015f206c0a6
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:13:28 -0500

    Update ValidationResult.java

--- src/packagee/validation/ValidationResult.java
+++ src/packagee/validation/ValidationResult.java
@@ -1,6 +1,4 @@
-/*
-hola
-*/
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
