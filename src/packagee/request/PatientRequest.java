83e7fa6d988b8b782aab5582342207d9302f1150
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:30:26 -0500

    Update PatientRequest.java

--- src/packagee/request/PatientRequest.java
+++ src/packagee/request/PatientRequest.java
@@ -1,13 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.request;
 
-/**
- *
- * @author danip
- */
 public class PatientRequest {
-    
+
+    private final String id;
+    private final String username;
+    private final String firstname;
+    private final String lastname;
+    private final String password;
+    private final String passwordConfirmation;
+    private final String email;
+    private final String birthdate;
+    private final String gender;
+    private final String phone;
+    private final String address;
+
+    public PatientRequest(String id, String username, String firstname, String lastname,
+                          String password, String passwordConfirmation, String email,
+                          String birthdate, String gender, String phone, String address) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.passwordConfirmation = passwordConfirmation;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
+
