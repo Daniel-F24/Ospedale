f2ef307a8602b443ab7180e52ca9dc74ae5ddb34
Daniel-F24 <danyrespawn@gmail.com>
2026-05-24 17:49:32 -0500

    Update IdGenerator.java

--- src/packagee/util/IdGenerator.java
+++ src/packagee/util/IdGenerator.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.util;
 
-/**
- *
- * @author danip
- */
+import packagee.repository.AppointmentRepository;
+import packagee.repository.HospitalizationRepository;
+
+
 public class IdGenerator {
-    
+
+    private final AppointmentRepository appointmentRepository;
+    private final HospitalizationRepository hospitalizationRepository;
+
+    public IdGenerator(AppointmentRepository appointmentRepository,
+                       HospitalizationRepository hospitalizationRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.hospitalizationRepository = hospitalizationRepository;
+    }
+
+    public String nextAppointmentId(long patientId) {
+        int sequence = appointmentRepository.countByPatientId(patientId);
+        String candidate;
+        do {
+            candidate = "A-" + patientId + "-" + String.format("%04d", sequence);
+            sequence++;
+        } while (appointmentRepository.existsById(candidate));
+        return candidate;
+    }
+
+    public String nextHospitalizationId(long patientId) {
+        int sequence = hospitalizationRepository.countByPatientId(patientId);
+        String candidate;
+        do {
+            candidate = "H-" + patientId + "-" + String.format("%04d", sequence);
+            sequence++;
+        } while (hospitalizationRepository.existsById(candidate));
+        return candidate;
+    }
 }
+
