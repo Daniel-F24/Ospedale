82265df683df218c9767153c76d89d99e2371309
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:26:05 -0500

    Update DoctorController.java

--- src/packagee/controller/DoctorController.java
+++ src/packagee/controller/DoctorController.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.controller;
 
-/**
- *
- * @author danip
- */
+import java.util.List;
+import packagee.dto.DoctorDTO;
+import packagee.model.enums.Specialty;
+import packagee.request.DoctorRequest;
+import packagee.response.Response;
+import packagee.service.DoctorService;
+
 public class DoctorController {
-    
+
+    private final DoctorService doctorService;
+
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+    public Response<DoctorDTO> registerDoctor(DoctorRequest request) {
+        return doctorService.registerDoctor(request);
+    }
+
+    public Response<DoctorDTO> updateDoctor(DoctorRequest request) {
+        return doctorService.updateDoctor(request);
+    }
+
+    public Response<DoctorDTO> getDoctorInfo(long doctorId) {
+        return doctorService.getDoctorById(doctorId);
+    }
+
+    public Response<List<DoctorDTO>> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    public Response<List<DoctorDTO>> getDoctorsBySpecialty(String specialtyText) {
+        return doctorService.getDoctorsBySpecialty(specialtyText);
+    }
+
+    public Specialty[] getAvailableSpecialties() {
+        return Specialty.values();
+    }
 }
+
