e8243da2490624a66e561548405d548493183ae4
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:29:11 -0500

    Update DoctorRequest.java

--- src/packagee/request/DoctorRequest.java
+++ src/packagee/request/DoctorRequest.java
@@ -1,14 +1,65 @@
 package packagee.request;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author danip
- */
 public class DoctorRequest {
-    
+
+    private final String id;
+    private final String username;
+    private final String firstname;
+    private final String lastname;
+    private final String password;
+    private final String passwordConfirmation;
+    private final String specialty;
+    private final String licenceNumber;
+    private final String assignedOffice;
+
+    public DoctorRequest(String id, String username, String firstname, String lastname,
+                         String password, String passwordConfirmation, String specialty,
+                         String licenceNumber, String assignedOffice) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.passwordConfirmation = passwordConfirmation;
+        this.specialty = specialty;
+        this.licenceNumber = licenceNumber;
+        this.assignedOffice = assignedOffice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public String getAssignedOffice() {
+        return assignedOffice;
+    }
 }
+
