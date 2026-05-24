2a1134ec4beb52fb3ea41d6579bd1dce71947ffe
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:23:21 -0500

    Update UserDTO.java

--- src/packagee/dto/UserDTO.java
+++ src/packagee/dto/UserDTO.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.dto;
 
-/**
- *
- * @author danip
- */
+
 public class UserDTO {
-    
+
+    private final long id;
+    private final String username;
+    private final String firstname;
+    private final String lastname;
+    private final String fullName;
+    private final String role;
+
+    public UserDTO(long id, String username, String firstname, String lastname, String role) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.fullName = firstname + " " + lastname;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
+
