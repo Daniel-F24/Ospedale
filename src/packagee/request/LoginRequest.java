89c58da4f8fd3924ebcd2cf9b29c0c5cb7b94d89
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:30:01 -0500

    Update LoginRequest.java

--- src/packagee/request/LoginRequest.java
+++ src/packagee/request/LoginRequest.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.request;
 
-/**
- *
- * @author danip
- */
 public class LoginRequest {
-    
+
+    private final String username;
+    private final String password;
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
