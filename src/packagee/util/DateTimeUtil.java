d0510964856f4dfc2602d16472f1f076ee4949cd
Daniel-F24 <danyrespawn@gmail.com>
2026-05-24 17:49:16 -0500

    Update DateTimeUtil.java

--- src/packagee/util/DateTimeUtil.java
+++ src/packagee/util/DateTimeUtil.java
@@ -1,14 +1,43 @@
 package packagee.util;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-/**
- *
- * @author danip
- */
-public class DateTimeUtil {
-    
+public final class DateTimeUtil {
+
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    private DateTimeUtil() {
+    }
+
+    public static LocalDate parseDate(String date) {
+        return LocalDate.parse(date, DATE_FORMATTER);
+    }
+
+    public static LocalTime parseTime(String time) {
+        return LocalTime.parse(time, TIME_FORMATTER);
+    }
+
+    public static LocalDateTime parseDateTime(String date, String time) {
+        return LocalDateTime.of(parseDate(date), parseTime(time));
+    }
+
+    public static String formatDate(LocalDate date) {
+        return date == null ? "" : date.format(DATE_FORMATTER);
+    }
+
+    public static String formatTime(LocalTime time) {
+        return time == null ? "" : time.format(TIME_FORMATTER);
+    }
+
+    public static String formatDateTime(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return "";
+        }
+        return formatDate(dateTime.toLocalDate()) + " " + formatTime(dateTime.toLocalTime());
+    }
 }
+
