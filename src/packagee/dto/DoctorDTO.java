16712edc05bbc295eaf63ea86b7cd03b523ff7d9
Sebastian Rivera <118506193+Chevinormalan@users.noreply.github.com>
2026-05-24 18:19:36 -0500

    Update DoctorDTO.java

--- src/packagee/dto/DoctorDTO.java
+++ src/packagee/dto/DoctorDTO.java
@@ -1,14 +1,31 @@
 package packagee.dto;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author danip
+ * Datos serializados del doctor para formularios, comboBox y tablas.
  */
-public class DoctorDTO {
-    
+public class DoctorDTO extends UserDTO {
+
+    private final String specialty;
+    private final String licenceNumber;
+    private final String assignedOffice;
+
+    public DoctorDTO(long id, String username, String firstname, String lastname,
+                     String specialty, String licenceNumber, String assignedOffice) {
+        super(id, username, firstname, lastname, "DOCTOR");
+        this.specialty = specialty;
+        this.licenceNumber = licenceNumber;
+        this.assignedOffice = assignedOffice;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public String getAssignedOffice() {
+        return assignedOffice;
+    }
 }
