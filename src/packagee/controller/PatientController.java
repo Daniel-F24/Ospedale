b3c1c069f982d560009eec97e4ec046f416bedba
gabrielatorresgo <gabrielat@uninorte.edu.co>
2026-05-24 18:27:03 -0500

    Update PatientController.java

--- src/packagee/controller/PatientController.java
+++ src/packagee/controller/PatientController.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package packagee.controller;
 
-/**
- *
- * @author danip
- */
+import java.util.List;
+import packagee.dto.PatientDTO;
+import packagee.request.PatientRequest;
+import packagee.response.Response;
+import packagee.service.PatientService;
+
 public class PatientController {
-    
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    public Response<PatientDTO> registerPatient(PatientRequest request) {
+        return patientService.registerPatient(request);
+    }
+
+    public Response<PatientDTO> updatePatient(PatientRequest request) {
+        return patientService.updatePatient(request);
+    }
+
+    public Response<PatientDTO> getPatientInfo(long patientId) {
+        return patientService.getPatientById(patientId);
+    }
+
+    public Response<List<PatientDTO>> getAllPatients() {
+        return patientService.getAllPatients();
+    }
 }
